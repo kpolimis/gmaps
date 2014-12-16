091eb010226817be8c50c86a215d72a600bedd4f
Pascal Bugnion <pascal@bugnion.org>
2014-12-16 14:54:52 +0000

    Added template file to nbconvert config.

--- docs/ipython_nbconvert_config.py
+++ docs/ipython_nbconvert_config.py
@@ -3,3 +3,5 @@ conf = get_config()
 
 conf.Exporter.preprocessors = [ "gmaps_preprocessors.SuppressOutputPreprocessor", 
     "gmaps_preprocessors.ModelInsertPreprocessor" ]
+
+conf.TemplateExporter.template_file = "templates/custom.tpl"
